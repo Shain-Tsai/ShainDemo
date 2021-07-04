ce80e242abd7460dbbeb0109e276bfa55809aba2
烜廷 蔡 <shain.tsai@zerone.com.tw>
2021-07-05 04:51:50 +0800

    新增task、launch以及驗證

--- WebSite/src/components/HelloWorld.vue
+++ WebSite/src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <a href="/.auth/login/aad">Login</a>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
